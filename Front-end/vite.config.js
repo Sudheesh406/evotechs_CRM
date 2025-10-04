b2221c632d7adb438aca1ee0ab22ea4f9affe314
Sudheesh406 <sudheeshunni406@gmail.com>
2025-10-04 10:09:02 +0530

    completed

--- Front-end/vite.config.js
+++ Front-end/vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   server: {
-    host: true, // expose to network
-    port: Number(process.env.PORT) || 5173, // use Render's PORT env
+    host: true, // needed for dev server, optional for preview
+    port: Number(process.env.PORT) || 5173,
+  },
+  preview: {
+    host: true, // allow network hosts
+    port: Number(process.env.PORT) || 4173,
+    allowedHosts: ["evotechs-crm-front-end.onrender.com"], // add your domain here
   },
   build: {
-    outDir: 'dist', // default Vite build folder
+    outDir: "dist",
   },
-})
+});
