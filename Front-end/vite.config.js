782d19a360faca61f2229ce217b2fbf2a3ac53b1
Sudheesh406 <sudheeshunni406@gmail.com>
2025-10-04 09:58:31 +0530

    completed

--- Front-end/vite.config.js
+++ Front-end/vite.config.js
@@ -5,4 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  server: {
+    host: true, // expose to network
+    port: Number(process.env.PORT) || 5173, // use Render's PORT env
+  },
+  build: {
+    outDir: 'dist', // default Vite build folder
+  },
 })
