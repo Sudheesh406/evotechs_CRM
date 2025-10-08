4cd11afca82fb75505607b692d0105b71dd0dec3
Sudheesh406 <sudheeshunni406@gmail.com>
2025-10-08 12:05:56 +0530

    testing

--- Back-end/controller/v1/messageController.js
+++ Back-end/controller/v1/messageController.js
@@ -4,7 +4,6 @@ const signup = require("../../models/v1/Authentication/authModel");
 const roleChecker = require("../../utils/v1/roleChecker");
 const { Op } = require("sequelize");
 
-
 const createMessages = async (data) => {
   try {
     const { senderId, receiverId, message } = data;
@@ -13,15 +12,17 @@ const createMessages = async (data) => {
     if (!senderId || !receiverId || !message) {
       throw new Error("Missing required fields");
     }
-    
+
     const { text, time, date } = message;
+    const parts = date.split("-"); // ["08", "10", "2025"]
+    const rotated = `${parts[2]}-${parts[1]}-${parts[0]}`;
 
     const admin = await roleChecker(senderId);
 
     const newMessage = await messages.create({
       message: text,
-      sendingTime : time,
-      sendingDate : date,
+      sendingTime: time,
+      sendingDate: rotated,
       receiverId,
       senderId,
       isAdmin: admin,
@@ -32,9 +33,6 @@ const createMessages = async (data) => {
     console.error("Error in createMessages:", error);
   }
 };
-
-
-
 
 const getMessages = async (req, res) => {
   try {
@@ -48,23 +46,19 @@ const getMessages = async (req, res) => {
           { senderId: id, receiverId: user.id },
         ],
       },
-      order: [
-        ["sendingDate", "ASC"],  // first sort by date
-        ["sendingTime", "ASC"],  // then by time
-      ],
-    });  
-
+    });
 
     const userId = user.id;
 
-    return httpSuccess(res, 201, "Messages fetched successfully", { existing, userId });
+    return httpSuccess(res, 200, "Messages fetched successfully", {
+      existing,
+      userId,
+    });
   } catch (error) {
     console.log("Error found in getting messages", error);
     return httpError(res, 500, "Server error", error.message || error);
   }
 };
-
-
 
 const getAllMembers = async (req, res) => {
   try {
@@ -90,6 +84,5 @@ const getAllMembers = async (req, res) => {
     return httpError(res, 500, "Server error", error.message || error);
   }
 };
-
 
 module.exports = { createMessages, getMessages, getAllMembers };
