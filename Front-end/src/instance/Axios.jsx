c28a4f4f65a4501af440b2e651e02cd95b82e99e
Sudheesh406 <sudheeshunni406@gmail.com>
2025-10-17 14:57:37 +0530

    rework updated

--- Front-end/src/instance/Axios.jsx
+++ Front-end/src/instance/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api/rese/`,  // ✅ use template literal
+  baseURL: '/api',  // ✅ use template literal
   headers: {
     'Content-Type': 'application/json',
   },
